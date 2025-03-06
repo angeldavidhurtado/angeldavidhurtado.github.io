03a65033ba8ec9b42ff0d31be49302a04957069f
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-03-06 17:33:02 -0500

    Ajustando background para muchos navegadores y muchas configuraciones

--- background-resize.js
+++ background-resize.js
@@ -3,7 +3,20 @@
 	const bg = $background;
 
 	const heightBackground = () => {
-		height = screen.height
+		/*
+		window.innerWidth // ancho de la web en px de css, así se desborde
+		h // altura pantalla en px de css
+		screen.width // ancho de la pantalla en px reales
+		screen.height // altura pantalla en px reales
+
+		aproximacion por encima de la altura visible y sea "constante" al
+		ocultarsen partes de la interfaz del navegador web movil
+		h = (screen.height / screen.width) * window.innerWidth
+		h *= 1.2 // tolerancia
+		*/
+		height = (screen.height / screen.width) * window.innerWidth * 1.2
+
+		height = height
 		bg.style.height = `${height}px`
 		document.body.style.marginTop = `-${height}px`
 	}

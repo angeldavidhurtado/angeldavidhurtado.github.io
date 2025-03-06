83382273d4ba1facc0fefa304407ebe186853a48
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-03-06 12:32:55 -0500

    Dando estilos finales a background ajustando luces para funcionamiento fluido en firefox

--- background-resize.js
+++ background-resize.js
@@ -3,21 +3,7 @@
 	const bg = $background;
 
 	const heightBackground = () => {
-		/*
-		creo que javascript no proporciona una manera
-		de optener la maxima altura visible de la web
-		lo que en css seria 100lvh, un forma que sea
-		precisa y disponible en todos los navegadores
-		web, el siguiente calculo para la variable
-		height es una aproximacion que he logrado
-		hacer con algunos metodos que proporciona
-		javascript y parecen funcionar de manera bastante
-		aproximada en diversos navegadores incluso en
-		movil con la opcion ver sitio de escritorio activada
-		*/
-		height = visualViewport.scale == 1
-			? screen.height
-			: Math.round((screen.height * visualViewport.width) / screen.width)
+		height = screen.height
 		bg.style.height = `${height}px`
 		document.body.style.marginTop = `-${height}px`
 	}

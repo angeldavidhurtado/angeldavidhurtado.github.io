a428261174995ff844499b94e78a3e3289ba3796
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-03-06 11:31:14 -0500

    Actuliando codigo

--- background-resize.js
+++ background-resize.js
@@ -1,8 +1,30 @@
-/*
 (function heightBackgroundHandler() {
+	let height;
+	const bg = $background;
 
+	const heightBackground = () => {
+		/*
+		creo que javascript no proporciona una manera
+		de optener la maxima altura visible de la web
+		lo que en css seria 100lvh, un forma que sea
+		precisa y disponible en todos los navegadores
+		web, el siguiente calculo para la variable
+		height es una aproximacion que he logrado
+		hacer con algunos metodos que proporciona
+		javascript y parecen funcionar de manera bastante
+		aproximada en diversos navegadores incluso en
+		movil con la opcion ver sitio de escritorio activada
+		*/
+		height = visualViewport.scale == 1
+			? screen.height
+			: Math.round((screen.height * visualViewport.width) / screen.width)
+		bg.style.height = `${height}px`
+		document.body.style.marginTop = `-${height}px`
+	}
+	heightBackground()
+
+	window.addEventListener('resize', heightBackground)
 })()
-*/
 
 /*
 en style.css en la seccion
@@ -10,73 +32,3 @@ en style.css en la seccion
 estan los estilos que
 trabajan con este script
 */
-
-
-
-
-class HeightBackgroundHandler {
-	constructor(idContentBackground) {
-		this.lvh = new LargeViewportHeight()
-		this.bg = this.createBg()
-
-		const contentBg = document.getElementById(idContentBackground)
-		contentBg.prepend(this.bg)
-
-		window.addEventListener('load', this.setHeightBackground)
-		window.addEventListener('resize', this.setHeightBackground)
-	}
-
-	setHeightBackground = () => {
-		const height = this.lvh.getLargeViewportHeight()
-		this.bg.style.height = `${height}px`
-		// Esto se puede dejar para que no sea el body sino el mismo el mismo div y asi liberar los estilos en css por si quieren hacer algo en el futuro, incluso dejar solo el padre del bg para que este script lo cree y administre todo lo relacionado y liberar creo que completamente el css, este todo limpio y modularizado
-		document.body.style.marginTop = `-${height}px`
-	}
-
-	createBg = () => {
-		const bg = document.createElement('div')
-		bg.className = 'background'
-
-		const imgBackground = document.createElement('img')
-		imgBackground.setAttribute('src', './img/bg.png')
-		imgBackground.setAttribute('alt', '')
-
-		bg.appendChild(imgBackground)
-		return bg
-	}
-}
-
-
-
-
-class LargeViewportHeight {
-	constructor() {
-		this.divHeight = this.createDivHeight()
-	}
-
-	getLargeViewportHeight = () => {
-		document.body.appendChild(this.divHeight)
-		const height = this.divHeight.offsetHeight
-		document.body.removeChild(this.divHeight)
-		return height
-	}
-
-	createDivHeight = () => {
-		const divHeight = document.createElement('div')
-		divHeight.setAttribute('style', `
-			height: 100lvh;
-			margin: 0;
-			padding: 0;
-			border: 0;
-			position: absolute;
-			top: 0;
-			z-index: -1;
-		`)
-		return divHeight
-	}
-}
-
-
-
-
-const heightBackgroundHandler = new HeightBackgroundHandler('id-content-background')

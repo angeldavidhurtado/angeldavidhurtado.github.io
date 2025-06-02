c7c2a947c34e1747fe8f18d7428ab462a479d378
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-06-02 16:28:06 -0500

    previous version

---
+++ previous-versions/v2/background-resizer.js
@@ -0,0 +1,18 @@
+(function () {
+	const bgContent = document.getElementsByClassName('background')[0]
+
+	const setBackroundHeight = () => {
+		if (!bgContent) return
+		const height = Math.round(screen.height) * 1.2
+		/*
+		utiliza screen.height porque en movil al ocultarse
+		las barras del navegador no se produce un salto
+		brusco, sin que siempre se ve bien
+		*/
+		bgContent.style.height = `${height}px`
+		bgContent.style.marginTop = `-${height}px`
+	}
+
+	window.addEventListener('load', setBackroundHeight)
+	window.addEventListener('resize', setBackroundHeight)
+})()
